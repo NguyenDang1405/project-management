c7b9c4bb64d6eedc93bf210fa0388bd459b8db65
NguyenDang1405 <dangnguyen0175@gmail.com>
2025-07-24 20:49:34 +0700

    render_products

--- controller/clients/products.controller.js
+++ controller/clients/products.controller.js
@@ -1,6 +1,16 @@
-module.exports.index = (req, res) => {
+const Products = require("../../models/products.models")
+module.exports.index = async (req, res) => {
+    const products = await Products.find({
+        status: "active",
+        deleted: false
+    });
+    const newProducts = products.map(item => {
+        item.priceNew = (item.price*(100-item.discountPercentage)/100).toFixed(0);
+        return item;
+    })
+
     res.render("clients/pages/products/index", {
         pagTitle: "Danh sách sản phẩm",
-        message:"Đây là trang danh sách sản phẩm"
+        products: products
     })
 }
--- models/products.models.js
+++ models/products.models.js
@@ -7,7 +7,7 @@ const ProductSchema = new mongoose.Schema({
     stock: Number,
     thumbnail: Number,
     position: Number, // starts at 1
-    status: Number,
+    status: String,
     deleted: Boolean
 });
 
